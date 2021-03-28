b57b79163d747cdd7cfef72555a2a03c6ec5e336
Christian Toney <37517470+Draguwro@users.noreply.github.com>
2021-03-28 13:59:47 -0400

    Update RhythmService.lua

--- RhythmService.lua
+++ RhythmService.lua
@@ -2,9 +2,11 @@
 -- Created by Christian Toney / Draguwro
 
 local RunService = game:GetService("RunService");
-
 local RhythmService = {
-  Tolerance = {Perfect = 0.1, OK = 0.2}
+  Tolerance = {0.1, 0.2} 
+  -- The first tolerance level is considered a perfect, 
+  -- while the last tolerance level is considered right before the player misses the beat
+  -- You can add more tolerance levels if you'd like
 };
 local Song = {Sound = nil, Keys = {}, KeyPosition = 1, StopwatchEvent = nil};
 local Events = {};
@@ -62,26 +64,20 @@ function RhythmService:CheckRhythm()
   
   local SongPosition = Song.Sound.TimePosition;
   local Goal = Song.Keys[Song.KeyPosition];
-  local PerfectTolerance = RhythmService.Tolerance.Perfect;
-  local OKTolerance = RhythmService.Tolerance.OK;
   local Result = {
     GoalTime = Goal[1];
     HitTime = SongPosition;
   };
   
   -- Check the time
-  for level, tolerance in pairs(RhythmService.Tolerance) do
-    coroutine.wrap(function()
-      if Goal[2] ~= 0 then
-        if Goal[1] - tolerance <= SongPosition and SongPosition <= Goal[1] + tolerance then
-          Result.Rating = level;
-        end;
+  for level, tolerance in ipairs(RhythmService.Tolerance) do
+    if Goal[2] ~= 0 then
+      if Goal[1] - tolerance <= SongPosition and SongPosition <= Goal[1] + tolerance then
+        Result.Rating = level;
+        RhythmService:ToggleKey(true);
+        break;
       end;
-    end)();
-  end;
-  
-  if not Result.Rating then
-    RhythmService:ToggleKey(true);
+    end;
   end;
   
   return Result;
@@ -123,7 +119,7 @@ function RhythmService:StartStopwatch()
   -- Add a new SW
   Song.StopwatchEvent = RunService.Heartbeat:Connect(function()
     for i, v in ipairs(Song.Keys) do
-      if v[2] ~= 0 and v[1] + RhythmService.Tolerance.OK < Song.Sound.TimePosition then 
+      if v[2] ~= 0 and v[1] + RhythmService.Tolerance[#RhythmService.Tolerance] < Song.Sound.TimePosition then 
         RhythmService:ToggleKey(true);
         Events.OnIdle:Fire();
         break;
