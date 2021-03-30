39d116e4325b11248e1e14cb08090e76d7ea0d45
Christian Toney <37517470+Draguwro@users.noreply.github.com>
2021-03-30 16:20:56 -0400

    Update PrintAccuracy.lua

--- examples/PrintAccuracy.lua
+++ examples/PrintAccuracy.lua
@@ -2,7 +2,7 @@ local UserInputService = game:GetService("UserInputService");
 local RhythmService = require(script.RhythmService);
 
 RhythmService:SetSound(workspace.Sound, false, true); -- Assuming the sound is in the workspace
-RhythmService:SetKeys({0.95, 2.35, 3.95}); -- Certain parts of the sound to check
+RhythmService:SetKeys({0.95, 2.35, {3.95, 5}}); -- Certain parts of the sound to check
 RhythmService.OnIdle:Connect(function() 
   print("Miss (Idle)")
 end);
@@ -11,5 +11,16 @@ UserInputService.InputBegan:Connect(function(input)
   if input.KeyCode == Enum.KeyCode.Space then
     local Result = RhythmService:CheckRhythm();
     print((Result.Rating == 1 and "Perfect") or (Result.Rating == 2 and "OK") or "Miss");
+  end;
+end);
+
+UserInputService.InputEnded:Connect(function(input)
+  if input.KeyCode == Enum.KeyCode.Space then
+    if RhythmService.Stopwatch then
+      local Result = RhythmService:CheckRhythm(true);
+      if Result then
+        print((Result.Rating == 1 and "Perfect hold!") or (Result.Rating == 2 and "OK hold") or "Miss");
+      end;
+    end;
   end;
 end);
