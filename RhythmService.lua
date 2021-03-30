602552527a0aa9e39c191c778d93759e2ba9292a
Christian Toney <37517470+Draguwro@users.noreply.github.com>
2021-03-30 16:33:24 -0400

    Update RhythmService.lua

--- RhythmService.lua
+++ RhythmService.lua
@@ -9,15 +9,16 @@ local RhythmService = {
   -- You can add more tolerance levels if you'd like
   Stopwatch = nil;
   -- You can use RhythmService.Stopwatch to see if the stopwatch is active
+  Sound = {Instance = nil, Keys = {}, KeyPosition = 1};
 };
-local Song = {Sound = nil, Keys = {}, KeyPosition = 1};
+
 local Events = {};
 
 function RhythmService:SetSound(sound: Sound, keepKeys: boolean?, startStopwatchOnPlay: boolean?)
   -- Set sound and remove keys if necessary
-  Song.Sound = sound;
+  RhythmService.Sound.Instance = sound;
   if not keepKeys then
-    Song.Keys = {};
+    RhythmService.Sound.Keys = {};
   end;
   
   if startStopwatchOnPlay then
@@ -30,45 +31,45 @@ function RhythmService:SetSound(sound: Sound, keepKeys: boolean?, startStopwatch
 end;
 
 function RhythmService:AddKey(timePosition: number, index: number?, endHold: number?)
-  assert(Song.Sound, "A sound instance must be defined before adding a key");
+  assert(RhythmService.Sound.Instance, "A sound instance must be defined before adding a key");
   
   -- Add key
   local Key = {timePosition, 1, endHold or nil};
   if index then
-    table.insert(Song.Keys, index, Key);
+    table.insert(RhythmService.Sound.Keys, index, Key);
   else
-    table.insert(Song.Keys, Key);
+    table.insert(RhythmService.Sound.Keys, Key);
   end;
 end;
 
 -- function RhythmService:SetKeys(keys: {{number, boolean?}}?)
 function RhythmService:SetKeys(keys)
   -- Set keys
-  Song.Keys = {};
+  RhythmService.Sound.Keys = {};
   if keys then
     for _, v in ipairs(keys) do
       if typeof(v) == "table" then
-        table.insert(Song.Keys, {v[1], 1, v[2]});
+        table.insert(RhythmService.Sound.Keys, {v[1], 1, v[2]});
       else
-        table.insert(Song.Keys, {v, 1});
+        table.insert(RhythmService.Sound.Keys, {v, 1});
       end;
     end
   end;
 end;
 
 function RhythmService:RemoveKey(index: number)
-  table.remove(Song.Keys, index);
+  table.remove(RhythmService.Sound.Keys, index);
 end;
 
 function RhythmService:CheckRhythm(noHold: boolean?)
-  assert(#Song.Keys > 0, "There has to be at least one key!");
+  assert(#RhythmService.Sound.Keys > 0, "There has to be at least one key!");
   assert(RhythmService.Stopwatch and RhythmService.Stopwatch.Connected, "The stopwatch hasn't started!");
   
-  local SongPosition = Song.Sound.TimePosition;
-  local Goal = Song.Keys[Song.KeyPosition];
+  local SoundPosition = RhythmService.Sound.Instance.TimePosition;
+  local Goal = RhythmService.Sound.Keys[RhythmService.Sound.KeyPosition];
   local Result = {
     GoalTime = (Goal[2] == 0 and noHold and Goal[3]) or Goal[1];
-    HitTime = SongPosition;
+    HitTime = SoundPosition;
   };
   
   if (noHold and not Goal[3]) or (noHold and Goal[2] == 1) then
@@ -78,12 +79,12 @@ function RhythmService:CheckRhythm(noHold: boolean?)
   -- Check the time
   for level, tolerance in ipairs(RhythmService.Tolerance) do
     if Goal[2] ~= 0 or (Goal[2] == 0 and noHold) then
-      if ((noHold and Goal[3]) or Goal[1]) - tolerance <= SongPosition and SongPosition <= ((noHold and Goal[3]) or Goal[1]) + tolerance then
+      if ((noHold and Goal[3]) or Goal[1]) - tolerance <= SoundPosition and SoundPosition <= ((noHold and Goal[3]) or Goal[1]) + tolerance then
         Result.Rating = level;
         if (noHold and Goal[3]) or (not noHold and not Goal[3]) then
           RhythmService:ToggleKey(true);
         else
-          Song.Keys[Song.KeyPosition][2] = 0;
+          RhythmService.Sound.Keys[RhythmService.Sound.KeyPosition][2] = 0;
         end;
         break;
       end;
@@ -95,17 +96,17 @@ end;
 
 function RhythmService:ToggleKey(disable: boolean?, index: number?, keepPosition: boolean?)
   -- Toggle key and shift position
-  Song.Keys[Song.KeyPosition or index][2] = (disable and 0) or 1;
-  if not keepPosition and #Song.Keys >= Song.KeyPosition + 1 then
-    Song.KeyPosition = Song.KeyPosition + 1;
-  elseif #Song.Keys < Song.KeyPosition + 2 then
+  RhythmService.Sound.Keys[RhythmService.Sound.KeyPosition or index][2] = (disable and 0) or 1;
+  if not keepPosition and #RhythmService.Sound.Keys >= RhythmService.Sound.KeyPosition + 1 then
+    RhythmService.Sound.KeyPosition = RhythmService.Sound.KeyPosition + 1;
+  elseif #RhythmService.Sound.Keys < RhythmService.Sound.KeyPosition + 2 then
     RhythmService:StopStopwatch();
   end;
 end;
 
 function RhythmService:ResetKeys()
-  for i, key in ipairs(Song.Keys) do
-    Song.Keys[i] = {key[1], 1, key[3]};
+  for i, key in ipairs(RhythmService.Sound.Keys) do
+    RhythmService.Sound.Keys[i] = {key[1], 1, key[3]};
   end;
 end;
 
@@ -118,18 +119,18 @@ function RhythmService:StopStopwatch()
 end;
 
 function RhythmService:StartStopwatch()
-  assert(Song.Sound, "A sound hasn't been defined!");
-  assert(#Song.Keys > 0, "There has to be at least one key!");
+  assert(RhythmService.Sound.Instance, "A sound hasn't been defined!");
+  assert(#RhythmService.Sound.Keys > 0, "There has to be at least one key!");
   
   RhythmService:StopStopwatch();
   RhythmService:ResetKeys();
-  Song.KeyPosition = 1;
+  RhythmService.Sound.KeyPosition = 1;
   
   -- Add a new SW
   RhythmService.Stopwatch = RunService.Heartbeat:Connect(function()
-    for i, v in ipairs(Song.Keys) do
+    for i, v in ipairs(RhythmService.Sound.Keys) do
       local Limit = (v[2] ~= 0 and v[1]) or v[3]
-      if Limit and Limit + RhythmService.Tolerance[#RhythmService.Tolerance] < Song.Sound.TimePosition then
+      if Limit and Limit + RhythmService.Tolerance[#RhythmService.Tolerance] < RhythmService.Sound.Instance.TimePosition then
         RhythmService:ToggleKey(true);
         Events.OnIdle:Fire();
         break;
